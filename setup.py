672130b150b7cf8b979c7a0845b02e13270a8a32
joao.teixeira <joao.teixeira@farfetch.com>
2021-03-10 08:20:36 +0000

    Change project name.

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pyguard',
+    name='python-guard',
     packages=find_packages(),
     python_requires='>3.0',
-    version='0.0.1',
+    version='0.1.0',
     author='Joao Teixeira',
+    author_email='ervilhaman@hotmail.com',
     url='https://github.com/joaoteixeira88/pyguard',
-    license='Copyright',
-    author_email='joaopteixeira58@gmail.com',
+    download_url='https://github.com/joaoteixeira88/pyguard/archive/v0.1.0.tar.gz',
+    keywords=['python', 'guard', 'parameters', 'exceptions'],
+    license='MIT',
     description='Guard is a fluent argument validation library that is intuitive, fast and extensible.',
-    install_requires=open('requirements.txt').readlines()
+    install_requires=open('requirements.txt').readlines(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
