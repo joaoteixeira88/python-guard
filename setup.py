a2deadf5c45be69b2a0a34583f054955f21d6b76
joao.teixeira <joao.teixeira@farfetch.com>
2021-03-10 09:18:02 +0000

    Add long description.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='python-guard',
     packages=find_packages(),
@@ -7,11 +12,13 @@ setup(
     version='0.1.0',
     author='Joao Teixeira',
     author_email='ervilhaman@hotmail.com',
-    url='https://github.com/joaoteixeira88/pyguard',
-    download_url='https://github.com/joaoteixeira88/pyguard/archive/v0.1.0.tar.gz',
+    url='https://github.com/joaoteixeira88/python-guard',
+    download_url='https://github.com/joaoteixeira88/python-guard/archive/0.1.0.tar.gz',
     keywords=['python', 'guard', 'parameters', 'exceptions'],
     license='MIT',
     description='Guard is a fluent argument validation library that is intuitive, fast and extensible.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=open('requirements.txt').readlines(),
     classifiers=[
         'Development Status :: 3 - Alpha',
